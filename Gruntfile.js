e4a6fe964b5e6aed7ac042737245672e163c8ae0
Geraint White <geraint@android.net>
2014-05-22 12:15:49 +0100

    Added Google tracking script

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
         },
         files: {
           'build/js/main.min.js': [
+            'src/js/google.js',
             'src/js/utils.js',
             'src/js/resources.js',
             'src/js/selectors.js',
