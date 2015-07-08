68bddd11bc0c200ab3b3219147047c80a7837622
Geraint White <geraintwhite@gmail.com>
2015-07-08 22:02:50 +0100

    Add tab keypress listener - fix #12

--- src/js/listeners.js
+++ src/js/listeners.js
@@ -73,3 +73,16 @@ addEvent(document, 'keydown', function(e) {
     }
   };
 });
+
+addEvent(edit_box, 'keydown', function (e) {
+  if (e.keyCode === 9) {
+    var start = this.selectionStart;
+    var end = this.selectionEnd;
+    var value = e.target.value;
+
+    e.target.value = value.substring(0, start) + '\t' + value.substring(end);
+    this.selectionStart = this.selectionEnd = start + 1;
+
+    e.preventDefault();
+  }
+});
